198c9d3920ce74a8ffcf38f53378548245ac6e5a
Tan Nguyen <74261778+tannlde@users.noreply.github.com>
2022-01-20 20:36:54 +0700

    Remove unnecessary library

--- Exercise 3/Student.cs
+++ Exercise 3/Student.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
+﻿
 namespace Exercise_3
 {
     class Student
